16dea0239a2e0c09dd4cb1b0784801188f70a822
sonia-deni <sonia_deni@hotmail.com>
2023-02-25 16:54:39 +0000

    added functions to intern and engineer to return additional information or override the getRole function inherited from Employee

--- index.js
+++ index.js
@@ -12,4 +12,52 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+const questions = () =>
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'managerName',
+      message: 'Please enter the team manager name',
+      validate: (managerName) => {if(managerName.length>0){return true} else{console.log('Cannot be empty'); return false}}
+    },
+    {
+      type: 'input',
+      name: 'managerEmployeeID',
+      message: 'Please enter the team manager Employee ID',
+      validate: (managerEmployeeID) => {if(managerEmployeeID.length>0){return true} else{console.log('Cannot be empty'); return false}}
+    },
+    {
+      type: 'input',
+      name: 'managerEmail',
+      message: 'Please enter the team manager email address',
+      validate: (managerEmail) => {if(managerEmail.length>0){return true} else{console.log('Cannot be empty'); return false}}
+    },
+    {
+      type: 'input',
+      name: 'managerOfficeNo',
+      message: 'Please enter the team manager office number',
+      validate: (managerOfficeNo) => {if(managerOfficeNo.length>0){return true} else{console.log('Cannot be empty'); return false}}
+    },
+    {
+      type: 'rawlist',
+      name: 'memberToAdd',
+      message: 'Please select employee type to add to the team',
+      choices: ['engineer', 'intern', 'team is complete']
+    },
+   
+  ]);
 
+
+const init = async () => {
+   
+    const answers = await questions();
+       
+    const initialInput = (answers);
+    //console.log(initialInput);
+
+    const managerCard = new Manager(answers.managerName, answers.managerEmployeeID, answers.managerEmail, answers.managerOfficeNo);
+    console.log(managerCard);
+  };
+
+
+init();
