b275691c00fdf975f60fb0cb3fb54251dc2d95e5
sonia-deni <sonia_deni@hotmail.com>
2023-02-25 21:47:46 +0000

    created prompts for each employee type and add to array of employee objects

--- index.js
+++ index.js
@@ -9,55 +9,116 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
-
+const team = [];
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
-const questions = () =>
-  inquirer.prompt([
-    {
-      type: 'input',
-      name: 'managerName',
-      message: 'Please enter the team manager name',
-      validate: (managerName) => {if(managerName.length>0){return true} else{console.log('Cannot be empty'); return false}}
-    },
-    {
-      type: 'input',
-      name: 'managerEmployeeID',
-      message: 'Please enter the team manager Employee ID',
-      validate: (managerEmployeeID) => {if(managerEmployeeID.length>0){return true} else{console.log('Cannot be empty'); return false}}
-    },
-    {
-      type: 'input',
-      name: 'managerEmail',
-      message: 'Please enter the team manager email address',
-      validate: (managerEmail) => {if(managerEmail.length>0){return true} else{console.log('Cannot be empty'); return false}}
-    },
-    {
-      type: 'input',
-      name: 'managerOfficeNo',
-      message: 'Please enter the team manager office number',
-      validate: (managerOfficeNo) => {if(managerOfficeNo.length>0){return true} else{console.log('Cannot be empty'); return false}}
-    },
-    {
-      type: 'rawlist',
-      name: 'memberToAdd',
-      message: 'Please select employee type to add to the team',
-      choices: ['engineer', 'intern', 'team is complete']
-    },
+
+const employeeQuestions = () => {
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Please enter name',
+                validate: (name) => {if(name.length>0){return true} else{console.log('Cannot be empty'); return false}}
+              },
+              {
+                type: 'input',
+                name: 'employeeID',
+                message: 'Please enter Employee ID',
+                validate: (employeeID) => {if(employeeID.length>0){return true} else{console.log('Cannot be empty'); return false}}
+              },
+              {
+                type: 'input',
+                name: 'email',
+                message: 'Please enter email address',
+                validate: (email) => {if(email.length>0){return true} else{console.log('Cannot be empty'); return false}}
+              },
+            ]);
+}
+
+const createManager = async () => {
+    const employee = await employeeQuestions();
+     inquirer
+      .prompt([
+              {
+                type: 'input',
+                name: 'managerOfficeNo',
+                message: 'Please enter the team manager office number',
+                validate: (officeNo) => {if(officeNo.length>0){return true} else{console.log('Cannot be empty'); return false}}
+              }
+        ])
+      .then(val => {
+        employee.officeNumber = val.managerOfficeNo;
+        const teamManager = new Manager(employee.name, employee.employeeID, employee.email, employee.officeNumber);
+        team.push(teamManager);
+        console.log(team);
+        nextEmployeeType();
+      });
+    }
    
-  ]);
+
+  const createEngineer = async () => {
+    const nextEmployee = await employeeQuestions();
+    inquirer
+      .prompt([
+              {
+                type: 'input',
+                name: 'github',
+                message: 'Please enter github',
+                validate: (github) => {if(github.length>0){return true} else{console.log('Cannot be empty'); return false}}
+              }
+        ])
+      .then(val => {
+        nextEmployee.github = val.github;
+        const teamMember = new Engineer(nextEmployee.name, nextEmployee.employeeID, nextEmployee.email, nextEmployee.github);
+        console.log(teamMember);
+        team.push(teamMember);
+        nextEmployeeType();
+      });
+  }
 
 
-const init = async () => {
-   
-    const answers = await questions();
-       
-    const initialInput = (answers);
-    //console.log(initialInput);
+  const createIntern = async () => {
+    const nextEmployee = await employeeQuestions();
+    inquirer
+      .prompt([
+              {
+                type: 'input',
+                name: 'school',
+                message: 'Please enter school',
+                validate: (school) => {if(school.length>0){return true} else{console.log('Cannot be empty'); return false}}
+              }
+        ])
+      .then(val => {
+        nextEmployee.school = val.school;
+        const teamMember = new Intern(nextEmployee.name, nextEmployee.employeeID, nextEmployee.email, nextEmployee.school);
+        team.push(teamMember);
+        nextEmployeeType();
+      });
+  }
 
-    const managerCard = new Manager(answers.managerName, answers.managerEmployeeID, answers.managerEmail, answers.managerOfficeNo);
-    console.log(managerCard);
-  };
 
+  const nextEmployeeType = async () => { 
+    return inquirer
+        .prompt([
+            {
+                type: 'rawlist',
+                name: 'memberToAdd',
+                message: 'Please select employee type to add to the team',
+                choices: ['engineer', 'intern', 'team is complete']
+              }
+            ])
+            .then(val => {
+                if (val.memberToAdd === "engineer") {
+                  createEngineer();
+                } else if(val.memberToAdd === "intern"){
+                  createIntern();
+                } else{
+                    console.log("Team complete!");
+                    process.exit(0);
+                }
+              });
+}
 
-init();
+ createManager();
